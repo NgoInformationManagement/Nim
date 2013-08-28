b2c466a68014e6d2ae8dedb62d5b0c9298fef963
alanglade <alanglade@galilee.fr>
2013-08-28 13:34:55 +0200

    adding the mission model and mapping

---
+++ Model/Mission.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\MissionBundle\Model;
+
+use \Gedmo\Translatable\Translatable;
+
+class Mission implements Translatable
+{
+    protected $id;
+    protected $title;
+    protected $description;
+    protected $country;
+    protected $workers;
+    protected $locale;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param mixed $workers
+     */
+    public function setWorkers($workers)
+    {
+        $this->workers = $workers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWorkers()
+    {
+        return $this->workers;
+    }
+}
