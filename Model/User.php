103a01737a257864c0ab003444a4af89326a1396
aRn0D <arnaud.langlade@gmail.com>
2013-09-07 03:45:10 +0200

    Adding the user model

---
+++ Model/User.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\CoreBundle\Model;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+class User extends BaseUser
+{
+    protected $id;
+}
