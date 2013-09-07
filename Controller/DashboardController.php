6b22f9170b57fd52b62e856531dd7de3d7467d19
aRn0D <arnaud.langlade@gmail.com>
2013-09-07 15:25:48 +0200

    Adding dashboard (future default route)

---
+++ Controller/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\WebBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
