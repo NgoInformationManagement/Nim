c7e72336eacc1e21341d121c050ce246693505bb
aRn0D <arnaud.langlade@gmail.com>
2013-09-24 21:36:12 +0200

    Adding mission form type spec

---
+++ spec/NIM/MissionBundle/Form/Type/MissionTypeSpec.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\NIM\MissionBundle\Form\Type;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MissionTypeSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('mission', array('nim'));
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('NIM\MissionBundle\Form\Type\MissionType');
+    }
+
+    /**
+     * @param Symfony\Component\Form\FormBuilderInterface $builder
+     */
+    function it_should_build_mission_form($builder)
+    {
+        $builder
+            ->add('translations', 'a2lix_translations_gedmo',  Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($builder)
+        ;
+        $builder
+            ->add('country', 'country', Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($builder)
+        ;
+
+        $builder
+            ->add('startedAt', 'date', Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($builder)
+        ;
+
+        $builder
+            ->add('endedAt', 'date', Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($builder)
+        ;
+
+        $this->buildForm($builder, array());
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     */
+    function it_should_define_assigned_data_class($resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'mission',
+            'validation_groups' => array('nim')
+        ))->shouldBeCalled();
+
+        $this->setDefaultOptions($resolver);
+    }
+
+    function it_should_have_valid_name()
+    {
+        $this->getName()->shouldReturn('nim_mission');
+    }
+}
