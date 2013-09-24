43c344ab95a7a2d8999bf53cc896188bfa7853e9
aRn0D <arnaud.langlade@gmail.com>
2013-09-24 21:47:38 +0200

    Adding mission extension spec

---
+++ spec/NIM/MissionBundle/DependencyInjection/NIMMissionExtensionSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace spec\NIM\MissionBundle\DependencyInjection;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class NIMMissionExtensionSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('NIM\MissionBundle\DependencyInjection\NIMMissionExtension');
+    }
+
+    function it_should_extends_nim_extension()
+    {
+        $this->shouldHaveType('NIM\Component\DependencyInjection\NIMExtension');
+    }
+
+    function it_should_extends_symfony_extension()
+    {
+        $this->shouldHaveType('Symfony\Component\DependencyInjection\Extension\Extension');
+    }
+}
