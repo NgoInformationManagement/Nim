a05eedb8695fe5f01a0af4f504740f158979eee2
aRn0D <arnaud.langlade@gmail.com>
2013-12-23 19:34:05 +0100

    Fixing the build

--- Behat/FormCollectionTrait.php
+++ Behat/FormCollectionTrait.php
@@ -11,30 +11,100 @@
 
 namespace NIM\FormBundle\Behat;
 
-
 use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Exception\ExpectationException;
 
 trait FormCollectionTrait
 {
     /**
      * For example: I click "Add" to add an item to "Email"
      *
-     * @When /^I click "([^"]*)" to add an item to "([^"]*)"$/
+     * @When /^I (click|press) "([^"]*)" to add an item to "([^"]*)"$/
      */
-    public function iaddItemTo($button, $label)
+    public function iaddItemTo($type, $button, $label)
     {
-        $div = $this->getSession()->getPage()->find('css',
-            sprintf('div.form-group:contains("%s")', $label)
+        $buttons = $this->getSession()->getPage()->findAll('xpath',
+            sprintf(
+                '//div[contains(@data-form-type, "collection")]//legend[text()[contains(., "%s")]]//*[text()[contains(., "%s")] and contains(@data-form-collection, "add")]',
+                $label, $button
+            )
         );
 
-        if (null === $div) {
+        foreach ($buttons as $button) {
+            if ($button->isVisible()) {
+                if ($type == 'click') {
+                    $button->click();
+                } else {
+                    $button->press();
+                }
+            }
+        }
+    }
+
+    /**
+     * @Then /^I should see "([^"]*)" field error in the item #(\d+) of the "([^"]*)" collection$/
+     */
+    public function iShouldSeeFieldErrorInTheCollection($fieldLabel, $position, $collectionLabel)
+    {
+        $fieldset = $this->getCollectionContainer($collectionLabel);
+        $fieldLabel = $this->getLabelOfCollectionField($fieldLabel, $position);
+
+        $this->assertSession()->elementExists(
+            'xpath',
+            sprintf("//div[contains(@class, 'error')]//*[contains(@id, '%s')]", $fieldLabel),
+            $fieldset
+        );
+    }
+
+    /**
+     * @When /^I fill in "([^"]*)" in the item #(\d+) of the "([^"]*)" collection with "([^"]*)"$/
+     */
+    public function iFillFieldInTheCollectionWith($fieldLabel, $position, $collectionLabel, $value, $container = null)
+    {
+        $fieldset = $this->getCollectionContainer($collectionLabel, $container);
+        $fieldLabel = $this->getLabelOfCollectionField($fieldLabel, $position);
+
+        $field = $fieldset->find(
+            'xpath',
+            sprintf("//div[contains(@data-form-collection, 'list')]//*[contains(@id, '%s')]", $fieldLabel)
+        );
+
+        if (null === $field) {
             throw new ExpectationException(
-                sprintf('Collection field with "%s" as label does not exist', $label),
+                sprintf('Collection named %s has not a field named %s', $collectionLabel, $fieldLabel),
                 $this->getSession()
             );
         }
 
-        $this->iClickButtonIn($div,$button);
+        $field->setValue($value);
+    }
+
+    /**
+     * @When /^I delete the item #(\d+) of the collection "([^"]*)"$/
+     */
+    public function iDeleteTheItemOfTheCollection($position = 1, $collectionLabel)
+    {
+        $fieldset = $this->getCollectionContainer($collectionLabel);
+
+        $field = $fieldset->find(
+            'xpath',
+            sprintf(
+                "//div[contains(@data-form-collection, 'item') and position()=%d]//a[contains(@data-form-collection, 'delete')]",
+                $position
+            )
+        );
+
+        if (null === $field) {
+            throw new ExpectationException(
+                sprintf(
+                    'Collection named %s has not deletion button',
+                    $collectionLabel
+                ),
+                $this->getSession()
+            );
+        }
+
+        $field->click();
     }
 
     /**
@@ -50,41 +120,12 @@ trait FormCollectionTrait
 
         if (null === $div) {
             throw new ExpectationException(
-                sprintf('Collection is not found in the current tab'),
+                'Collection is not found in the current tab',
                 $this->getSession()
             );
         }
 
         $this->iClickButtonIn($div, $button);
-    }
-
-
-    /**
-     * For example: I click "Delete" to delete the "1" item from "Email"
-     *
-     * @When /^I click "([^"]*)" to delete the "([0-9])" item from "([^"]*)"$/
-     */
-    public function ideleteItemTo($button, $nb, $label)
-    {
-        $div = $this->getSession()->getPage()->find('css',
-            sprintf('div.form-group:contains("%s")', $label)
-        );
-
-        if (null === $div) {
-            throw new ExpectationException(
-                sprintf('Collection field with "%s" as label does not exist', $label),
-                $this->getSession()
-            );
-        }
-
-        // TODO : do not work
-        $locator = sprintf('button:nth-child(%d)', $nb, $button);
-
-        if ($div->has('css', $locator)) {
-            $div->find('css', $locator)->press();
-        } else {
-            $div->clickLink($button);
-        }
     }
 
     /**
@@ -100,5 +141,38 @@ trait FormCollectionTrait
         } else {
             $div->clickLink($button);
         }
+    }
+
+    /**
+     * @param $fieldLabel
+     * @param $position
+     * @return string
+     */
+    private function getLabelOfCollectionField($fieldLabel, $position)
+    {
+        return sprintf('%d_%s', $position - 1, strtolower($fieldLabel));
+    }
+
+    /**
+     * @param $collectionLabel
+     * @return mixed
+     */
+    private function getCollectionContainer($collectionLabel)
+    {
+        $fieldsets = $this->getSession()->getPage()->findAll(
+            'xpath',
+            sprintf("//legend[text()[contains(., '%s')]]/..", $collectionLabel)
+        );
+
+        foreach ($fieldsets as $fieldset) {
+            if ($fieldset->isVisible() && null !== $fieldset) {
+                return $fieldset;
+            }
+        }
+
+        throw new ExpectationException(
+            sprintf('Collection named %s does not exist', $collectionLabel),
+            $this->getSession()
+        );
     }
 }
