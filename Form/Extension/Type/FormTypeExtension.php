3fefa4f2598de78dde97956a4422159255632bc4
aRn0D <arnaud.langlade@gmail.com>
2014-01-02 23:40:01 +0100

    Adding legend attribute to form type

---
+++ Form/Extension/Type/FormTypeExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\FormBundle\Form\Extension\Type;
+
+use NIM\FormBundle\Form\FormToolsTrait;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FormTypeExtension extends AbstractTypeExtension
+{
+    use FormToolsTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(array(
+            'legend'
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'legend' => array('string'),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $this->addVarToFormViewFromOptions($view, $options, 'legend');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+}
