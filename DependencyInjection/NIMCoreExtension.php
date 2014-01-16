5da6d48fdf065955e182b6f5885626f0b158fbd5
aRn0D <arnaud.langlade@gmail.com>
2014-01-16 21:16:46 +0100

    SyliusResourceExtension can not be used

--- DependencyInjection/NIMCoreExtension.php
+++ DependencyInjection/NIMCoreExtension.php
@@ -13,12 +13,13 @@ namespace NIM\CoreBundle\DependencyInjection;
 
 use Sylius\Bundle\ResourceBundle\DependencyInjection\SyliusResourceExtension;
 use NIM\CoreBundle\NIMCoreBundle;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 
-class NIMCoreExtension extends SyliusResourceExtension
+class NIMCoreExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -52,5 +53,34 @@ class NIMCoreExtension extends SyliusResourceExtension
         }
 
         $loader->load(sprintf('driver/%s.xml', $driver));
+    }
+
+    /**
+     * Remap class parameters.
+     *
+     * @param array $classes
+     * @param ContainerBuilder $container
+     */
+    protected function mapClassParameters(array $classes, ContainerBuilder $container)
+    {
+        foreach ($classes as $model => $serviceClasses) {
+            foreach ($serviceClasses as $service => $class) {
+                $service = $service === 'form' ? 'form.type' : $service;
+                $container->setParameter(sprintf('nim.%s.%s.class', $service, $model), $class);
+            }
+        }
+    }
+
+    /**
+     * Remap validation group parameters.
+     *
+     * @param array $validationGroups
+     * @param ContainerBuilder $container
+     */
+    protected function mapValidationGroupParameters(array $validationGroups, ContainerBuilder $container)
+    {
+        foreach ($validationGroups as $model => $groups) {
+            $container->setParameter(sprintf('nim.validation_group.%s', $model), $groups);
+        }
     }
 }
