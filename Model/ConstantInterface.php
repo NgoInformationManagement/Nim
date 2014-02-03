bf56d5947729f08c54243f8270c23b18ca91b4a6
alanglade <alanglade@galilee.fr>
2014-02-03 13:18:05 +0100

    New constantInterface

---
+++ Model/ConstantInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\CoreBundle\Model;
+
+interface ConstantInterface
+{
+    /**
+     * Return all the types
+     *
+     * @param  null|string $translationDomain
+     * @return array
+     */
+    public static function getTypes($translationDomain = null);
+}
