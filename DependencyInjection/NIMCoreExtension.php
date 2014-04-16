5fca53798c6ece7adc0264567e76c9f282f8d576
Arnaud Langlade <alanglade@clever-age.com>
2014-04-16 12:46:41 +0200

    The core extension use the sylius extension

--- DependencyInjection/NIMCoreExtension.php
+++ DependencyInjection/NIMCoreExtension.php
@@ -11,14 +11,14 @@
 
 namespace NIM\CoreBundle\DependencyInjection;
 
-use NIM\CoreBundle\NIMCoreBundle;
-use Sylius\Bundle\ResourceBundle\DependencyInjection\SyliusResourceExtension;
+use Sylius\Bundle\ResourceBundle\DependencyInjection\BaseExtension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 
-class NIMCoreExtension extends SyliusResourceExtension
+class NIMCoreExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -35,51 +35,5 @@ class NIMCoreExtension extends SyliusResourceExtension
 
         $container->setParameter('nim_core.driver', $driver);
         $container->setParameter('nim_core.driver.'.$driver, true);
-    }
-
-    /**
-     * Load bundle driver.
-     *
-     * @param string        $driver
-     * @param XmlFileLoader $loader
-     *
-     * @throws \InvalidArgumentException
-     */
-    protected function loadDriver($driver, XmlFileLoader $loader)
-    {
-        if (!in_array($driver, NIMCoreBundle::getSupportedDrivers())) {
-            throw new \InvalidArgumentException(sprintf('Driver "%s" is unsupported by NIMCoreBundle.', $driver));
-        }
-
-        $loader->load(sprintf('driver/%s.xml', $driver));
-    }
-
-    /**
-     * Remap class parameters.
-     *
-     * @param array            $classes
-     * @param ContainerBuilder $container
-     */
-    protected function mapClassParameters(array $classes, ContainerBuilder $container)
-    {
-        foreach ($classes as $model => $serviceClasses) {
-            foreach ($serviceClasses as $service => $class) {
-                $service = $service === 'form' ? 'form.type' : $service;
-                $container->setParameter(sprintf('nim.%s.%s.class', $service, $model), $class);
-            }
-        }
-    }
-
-    /**
-     * Remap validation group parameters.
-     *
-     * @param array            $validationGroups
-     * @param ContainerBuilder $container
-     */
-    protected function mapValidationGroupParameters(array $validationGroups, ContainerBuilder $container)
-    {
-        foreach ($validationGroups as $model => $groups) {
-            $container->setParameter(sprintf('nim.validation_group.%s', $model), $groups);
-        }
     }
 }
