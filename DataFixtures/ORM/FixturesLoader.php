7711c5c269b03db344fe68dfb757defca4f9c89d
Arnaud Langlade <alanglade@clever-age.com>
2014-04-16 12:48:58 +0200

    Adding fixtures

---
+++ DataFixtures/ORM/FixturesLoader.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\CoreBundle\DataFixtures\ORM;
+
+use Faker\Factory as FakerFactory;
+use Faker\Generator;
+use Nelmio\Alice\Fixtures;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+
+class FixturesLoader extends DataFixtureLoader
+{
+    protected $fixtureDir = '../../Resources/fixtures';
+
+    /**
+     * Container.
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Faker.
+     *
+     * @var Generator
+     */
+    protected $faker;
+
+
+    public function __construct()
+    {
+        $this->faker = FakerFactory::create();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        return array(
+            $this->getFixturePath('users.yml'),
+            $this->getFixturePath('phones.yml'),
+            $this->getFixturePath('emails.yml'),
+            $this->getFixturePath('missions.yml'),
+            $this->getFixturePath('agencies.yml'),
+            $this->getFixturePath('passports.yml'),
+            $this->getFixturePath('visas.yml'),
+            $this->getFixturePath('contacts.yml'),
+            $this->getFixturePath('workers.yml'),
+        );
+    }
+
+    /**
+     * @param $filename
+     * @return string
+     */
+    protected function getFixturePath($filename)
+    {
+        return sprintf('%s/%s', $this->getFixturesRoot(), $filename);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDocumentPath()
+    {
+        return sprintf('%s/document', $this->getFixturesRoot());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFixturesRoot()
+    {
+        return sprintf('%s/%s', __DIR__, $this->fixtureDir);
+    }
+}
