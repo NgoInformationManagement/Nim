ed6a18eafaf8f41f5757e9c7cfd73f5aa9025ffd
Arnaud Langlade <alanglade@clever-age.com>
2014-04-16 12:45:53 +0200

    Adding target resolver entity

--- NIMCoreBundle.php
+++ NIMCoreBundle.php
@@ -11,19 +11,28 @@
 
 namespace NIM\CoreBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NIMCoreBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
     public static function getSupportedDrivers()
     {
         return array(
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver(
+            array(realpath(__DIR__ . '/Resources/config/doctrine/model') => 'NIM\CoreBundle\Model'),
+            array('doctrine.orm.entity_manager'), 'nim_core.driver.doctrine/orm')
         );
     }
 }
