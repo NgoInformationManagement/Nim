ba242e4046a32bb1607d96c125df143f356298f3
Arnaud Langlade <alanglade@clever-age.com>
2014-05-02 19:44:39 +0200

    Adding vaccine to wotker form

---
+++ Model/Core/EntityFormTypeInterface.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NIM\FormBundle\Model\Core;
+
+interface EntityFormTypeInterface
+{
+    /**
+     * Return an array of data for Entity Form Type
+     *
+     * @return array
+     */
+    public function getEntityFormTypeData();
+} 
